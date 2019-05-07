3712f94a57d486a059e2d754fe665c5fe78b1279
Stephen John Raymundo <bayan.mahal@live.com>
2019-05-07 11:38:36 +0800

    add admin route

---
+++ src/views/Admin.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  #admin
+    aside.adminDrawer Aside
+    main.adminMain Main
+      nav.adminNav Nav
+
+</template>
+
+<style lang="sass" scoped>
+#admin
+  display: flex
+  height: 100vh
+  width: 100vw
+  max-height: 100vh
+  max-width: 100vw
+
+.adminDrawer
+  width: 15rem
+  background-color: red
+</style>
+
