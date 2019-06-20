6214eecaebce1003f66921047dad54568f5f67aa
Stephen John Raymundo <bayan.mahal@live.com>
2019-06-20 15:25:32 +0800

    update changes

--- src/views/admin/AdminGallery.vue
+++ src/views/admin/AdminGallery.vue
@@ -28,11 +28,17 @@
           .selectedPost__action(v-show="$route.params.id == 'new'" @click="publish")
             fa(icon="save")
             span &nbsp Publish
-      div
-        input(type="text" :value="c_activePost.title" style="display: block")
-        input(type="text" :value="c_activePost.description" style="display: block")
+      div(style="padding: 1rem; background-color: white")
+        label Title
+        input.field(type="text" :value="c_activePost.title" style="display: block")
+        label Description
+        input.field(type="text" :value="c_activePost.description" style="display: block")
+        br
         .grid
-          img(:src="photo.url" v-for="photo in c_activePost.photos")
+          div.grid__img(style="display: flex; align-items: center; justify-content: center; cursor: pointer") + Add photos
+          img.grid__img(:src="photo.url" v-for="photo in c_activePost.photos")
+
+    div(style="fixed; width: 100vw; height: 100vh; background-color: red")
         
 
 </template>
@@ -95,13 +101,6 @@ export default {
   border-left: 4px solid transparent
   padding-bottom: 1rem
 
-  &:hover .galleryCard__highlightStar
-    color: #00000011
-
-.galleryCard.gallerycard--isHighlight
-  .galleryCard__highlightStar
-    color: orange !important
-
 .galleryCard.router-link-active
   border-left: 4px solid $color-primary
 
@@ -125,6 +124,27 @@ export default {
   &:active
     color: #00000033 !important
 
+input.field
+  width: 100%
+  padding: 0.5rem 0
+  border: none
+  border-bottom: 1px solid $color-layout-border
+  margin-bottom: 0.5rem
+  margin-top: 0.25rem
+  font-weight: 500
+  color: inherit
+  outline: none
+
+input.field::placeholder
+  font-weight: normal
+
+input.field:disabled
+  opacity: 0.5
+  background-color: transparent
+
+input:focus
+  border-bottom-color: $color-primary
+
 
 
 .galleryCard__title
@@ -135,6 +155,10 @@ export default {
   padding-right: 1rem
   opacity: 0.5
   font-size: 0.9rem
+
+.grid__img
+  background-color: #ccc
+  min-height: 5rem
 
 .selectedPost
   display: flex
@@ -180,6 +204,10 @@ export default {
 .grid
   display: grid
   grid-template-columns: 1fr 1fr 1fr
+  grid-gap: 1rem
 
+label
+  color: #00000088
+  font-size: 0.9rem
 
 </style>
